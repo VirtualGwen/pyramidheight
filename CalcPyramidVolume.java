fd0c20182a521530a2d1d31e0d48ff5b3567ea78
VirtualGwen <gvirtue@me.com>
2021-07-05 09:26:07 -0400

    Create CalcPyramidVolume.java

---
+++ CalcPyramidVolume.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CalcPyramidVolume {
+
+   public static double pyramidVolume(double baseLength, double baseWidth, double pyramidHeight) {
+      
+      double baseArea;
+      double volume;
+      
+      baseArea = baseWidth * baseLength;
+      volume = (baseArea * pyramidHeight * 1/3);
+      
+      return volume;
+   }
+
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      double userLength;
+      double userWidth;
+      double userHeight;
+
+      userLength = scnr.nextDouble();
+      userWidth = scnr.nextDouble();
+      userHeight = scnr.nextDouble();
+
+      System.out.println("Volume: " + pyramidVolume(userLength, userWidth, userHeight));
+   }
+}
